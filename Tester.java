950f008c0a2b9b1eb99b79ed03baaca29378d271
mayornic26 <114428885+mayornic26@users.noreply.github.com>
2025-02-05 09:35:48 -0500

    Create Tester.java

---
+++ Tester.java
@@ -0,0 +1,31 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+
+	public class Tester {
+		
+		public static void main(String[] args){
+		
+		ArrayList<Attendee> attList = new ArrayList<Attendee>();
+		
+		Attendee a1 = new Attendee("Mr Posh", 12);
+		attList.add(a1);
+		Attendee a2 = new Attendee("Mr Nico", 13);
+		attList.add(a2);
+		Attendee a3 = new Attendee("Mr Brady", 14);
+		attList.add(a3);
+		
+		System.out.print(attList);
+	
+		String searchName = "Mr Brady";
+		
+		for(Attendee i : attList){
+		 if(i.getName().equals(Mr Brady){
+		 
+		 }
+		
+		}
+	}
+ }
