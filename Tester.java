d15d0c5a6c40cb512723c0ec796ae7173a0ebc35
mayornic26 <114428885+mayornic26@users.noreply.github.com>
2025-02-07 10:55:55 -0500

    Update Tester.java

--- Tester.java
+++ Tester.java
@@ -1,3 +1,4 @@
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -8,24 +9,21 @@ import java.util.ArrayList;
 		
 		public static void main(String[] args){
 		
-		ArrayList<Attendee> attList = new ArrayList<Attendee>();
 		
-		Attendee a1 = new Attendee("Mr Posh", 12);
+		Partay p1 = new Partay(); 
+		p1.loadPeople();//called method for p1 object, which is an instance of partay
+		
+		
+	    /*
+		ArrayList<Attendee> attList = new ArrayList<Attendee>();
+		Attendee a1 = new Attendee("Mr", "Posh", 12);
 		attList.add(a1);
-		Attendee a2 = new Attendee("Mr Nico", 13);
+		Attendee a2 = new Attendee("Nico", "Mayoral", 13);
 		attList.add(a2);
-		Attendee a3 = new Attendee("Mr Brady", 14);
+		Attendee a3 = new Attendee("Brady", "O'Conner", 14);
 		attList.add(a3);
 		
 		System.out.print(attList);
-	
-		String searchName = "Mr Brady";
-		
-		for(Attendee i : attList){
-		 if(i.getName().equals(Mr Brady){
-		 
-		 }
-		
-		}
+		*/
 	}
  }
