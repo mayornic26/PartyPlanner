aac7e634fbb590201ef08e16cf80c344e7b2a3cb
mayornic26 <114428885+mayornic26@users.noreply.github.com>
2025-02-21 09:36:07 -0500

    Update Tester.java

--- Tester.java
+++ Tester.java
@@ -4,26 +4,24 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.ArrayList;
 
-
 	public class Tester {
-		
 		public static void main(String[] args){
 		
 		
-		Partay p1 = new Partay(); 
-		p1.loadPeople();//called method for p1 object, which is an instance of partay
-		p1.loadCompanies();
-		
-	    /*
-		ArrayList<Attendee> attList = new ArrayList<Attendee>();
-		Attendee a1 = new Attendee("Mr", "Posh", 12);
-		attList.add(a1);
-		Attendee a2 = new Attendee("Nico", "Mayoral", 13);
-		attList.add(a2);
-		Attendee a3 = new Attendee("Brady", "O'Conner", 14);
-		attList.add(a3);
-		
-		System.out.print(attList);
-		*/
+		Partay p1 = new Partay();
+		try {
+		   p1.loadPeople();//called method for p1 object, which is an instance of partay
+		   p1.check_user_amounts();	
+		   p1.registerperson();
+	       p1.loadCompanies();
+	       p1.place_users_table();
+	       System.out.println("Roster by Company:");
+           p1.print_rosters_by_company();
+		   System.out.println("Roster by Table:");
+		   p1.print_rosters_by_table();
+	    } catch (IllegalArgumentException e) {
+           System.out.println(e.getMessage());
+		}
+	    
 	}
  }
