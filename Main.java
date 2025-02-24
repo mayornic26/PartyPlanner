8e56cd58e6a5c507058d0f3648d5a59793c97c99
mayornic26 <114428885+mayornic26@users.noreply.github.com>
2025-02-23 22:26:14 -0500

    Update Main.java
    Final Final for Main

--- Main.java
+++ Main.java
@@ -29,16 +29,16 @@ import java.util.ArrayList;
 		   p1.loadPeople();//called method for p1 object, which is an instance of partay
 		   p1.check_user_amounts();	 //called method to check that there are are the right amount of people and companies in the files
 		   p1.registerperson();  //called method to register a new person manually
-	       p1.loadCompanies(); //called method to load companies from files
-	       p1.place_users_table(); // called method to place attendees on tables
-	       System.out.println("Roster by Company:");
-           p1.print_rosters_by_company(); // called method to print rosters per company
+	           p1.loadCompanies(); //called method to load companies from files
+	           p1.place_users_table(); // called method to place attendees on tables
+	           System.out.println("Roster by Company:");
+                   p1.print_rosters_by_company(); // called method to print rosters per company
 		   System.out.println("Roster by Table:");
 		   p1.print_rosters_by_table(); // called method to print rosters by table
-           p1.findperson();
+                   p1.findperson();
 		   
-	    } catch (IllegalArgumentException e) {
-           System.out.println(e.getMessage());
+	       } catch (IllegalArgumentException e) {
+                    System.out.println(e.getMessage());
 		}
 	}
  }
